b0cf4188053d91778f94e3bfeb8eca45e3225903
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-05-11 14:43:10 +1000

    Minify css via PostCSS and add vendor prefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var postcss = require("postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+// define plugins to use with PostCSS
+var plugins = [
+  autoprefixer(), // browserslist in package.json
+  cssnano() // minify css
+];
 // transform scss/sass files to css
 gulp.task("sass", function() {
   // take main scss file
   return gulp.src("src/flexy.scss") // flexy.scss by default
     // transform to css with gulp-sass plugin
     .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(plugins)) // postprocessing
     .pipe(gulp.dest('dist')); // output to dist folder
 });
